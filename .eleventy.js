db296b273a46747a8742509c149c01957e7c8f30
Diego Auyón <diegoeauyonh@gmail.com>
2021-01-28 14:12:46 -0600

    WIP

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const categorias = [
+    "1 Servicios públicos generales",
+    "3 Orden público y seguridad",
+    "4 Asuntos económicos",
+    "5 Protección del medio ambiente",
+    "6 Vivienda y servicios comunitarios",
+    "7 Salud",
+    "9 Educación"
+];
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.setTemplateFormats('css,png,jpg,njk,html,otf,ttf');
+
+    categorias.forEach((categoria) => {
+        eleventyConfig.addCollection(categoria, function (collectionApi) {
+
+            let collection = collectionApi.getFilteredByTags('servicio').filter(function (item) {
+                return item.data.servicio.categoria === categoria;
+            });
+
+            return collection;
+        });
+    });
+}
