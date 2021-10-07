d79b9d08e4e132265dbb29620d6ae9f8490fa938
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2021-10-07 12:52:52 -0600

    cambio de base de datos

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,13 @@ const searchFilter = require('./filters/searchFilter');
 const searchFilterUE = require('./filters/searchFilterUE');
 
 const categorias = [
-    "1 Servicios públicos generales",
-    "3 Orden público y seguridad",
-    "4 Asuntos económicos",
-    "5 Protección del medio ambiente",
-    "6 Vivienda y servicios comunitarios",
-    "7 Salud",
-    "9 Educación"
+    "Servicios públicos generales",
+    "Orden público y seguridad",
+    "Asuntos económicos",
+    "Protección del medio ambiente",
+    "Vivienda y servicios comunitarios",
+    "Salud",
+    "Educación"
 ];
 
 module.exports = function (eleventyConfig) {
@@ -18,7 +18,7 @@ module.exports = function (eleventyConfig) {
         eleventyConfig.addCollection(categoria, function (collectionApi) {
 
             let collection = collectionApi.getFilteredByTags('servicio').filter(function (item) {
-                return item.data.servicio.categoria === categoria;
+                return item.data.servicio.Categoria === categoria;
             });
 
             return collection;
