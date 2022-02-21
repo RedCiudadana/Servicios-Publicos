3b5ded43c8ee534a82954ce63b48776eced05638
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2022-02-21 09:44:20 -0600

    arreglo final cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 const searchFilter = require('./filters/searchFilter');
 const searchFilterUE = require('./filters/searchFilterUE');
 
+const moment = require("moment");
+const now = new Date();
+
 const categorias = [
     "Servicios públicos generales",
     "Orden público y seguridad",
@@ -12,7 +15,15 @@ const categorias = [
 ];
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.setTemplateFormats('css,png,jpg,njk,html,otf,ttf');
+    
+    eleventyConfig.setTemplateFormats("njk,html,md");
+    
+    eleventyConfig.addPassthroughCopy('src');
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('elementos');
+    eleventyConfig.addPassthroughCopy('images');
+    eleventyConfig.addPassthroughCopy('admin');
 
     categorias.forEach((categoria) => {
         eleventyConfig.addCollection(categoria, function (collectionApi) {
@@ -40,4 +51,13 @@ module.exports = function (eleventyConfig) {
 
     
     eleventyConfig.addFilter("search2", searchFilterUE);
+
+    eleventyConfig.addNunjucksFilter("limit", function(array, limit) {
+        return array.slice(0, limit);
+    });
+
+    eleventyConfig.addFilter("dateFormat", function(date, format) {
+        return moment(date).format(format);
+    });
+
 }
