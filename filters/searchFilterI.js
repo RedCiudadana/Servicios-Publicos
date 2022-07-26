19d752e5029d498d14b2814e76fe65c76adf9fa7
unknown <josemarianoreyesh02@gmail.com>
2022-07-26 10:42:18 -0600

    buscador de instituciones avanzado

--- filters/searchFilterI.js
+++ filters/searchFilterI.js
@@ -4,6 +4,7 @@ module.exports = function (collection) {
     // what fields we'd like our index to consist of
     var index = elasticlunr(function () {
         this.addField("nombre");
+        this.addField("type");
         this.setRef("id");
     });
 
@@ -12,6 +13,7 @@ module.exports = function (collection) {
         index.addDoc({
             id: item.url,
             nombre: item.data.instituciones.name,
+            type: item.data.instituciones.type,
         });
     });
 
